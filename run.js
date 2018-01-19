41068932b7dc833e627d8e3205535efec0a7fc5f
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2018-01-18 16:14:35 -0800

    convert to typescript plugin

--- run.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-'use strict'
-
-const dirs = process.argv.slice(2) || process.cwd()
-const Setsumeisho = require('./src/setsumeisho')
-let instructions = Setsumeisho.build(dirs)
-console.log(instructions)
